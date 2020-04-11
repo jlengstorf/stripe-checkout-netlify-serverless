f83b52c646e468207d9d91468285024170576683
Jason Lengstorf <jason@lengstorf.com>
2020-04-10 18:58:23 -0700

    feat: display products using a template

--- public/js/load-products.js
+++ public/js/load-products.js
@@ -1,3 +1,22 @@
+function createProductFromTemplate(item) {
+  const template = document.querySelector('#product');
+  const product = template.content.cloneNode(true);
+
+  product.querySelector('h2').innerText = item.name;
+  product.querySelector('.description').innerText = item.description;
+  product.querySelector('[name=sku]').value = item.sku;
+  product.querySelector('.price').innerText = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: item.currency,
+  }).format((item.amount / 100).toFixed(2));
+
+  const img = product.querySelector('img');
+  img.src = item.image;
+  img.alt = item.name;
+
+  return product;
+}
+
 export async function loadProducts() {
   const data = await fetch('/.netlify/functions/get-products')
     .then((res) => res.json())
@@ -5,9 +24,9 @@ export async function loadProducts() {
 
   const products = document.querySelector('.products');
 
-  // TODO add markup to display products
-  const pre = document.createElement('pre');
-  pre.innerText = JSON.stringify(data, null, 2);
+  data.forEach((item) => {
+    const product = createProductFromTemplate(item);
 
-  products.appendChild(pre);
+    products.appendChild(product);
+  });
 }
